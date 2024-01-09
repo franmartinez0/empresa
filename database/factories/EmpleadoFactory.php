d175b2e4948bd757c8385559ed48d0376b4f05b5
franmartinez0 <fran.martinezlopez29398@gmail.com>
2024-01-09 18:01:58 +0100

    empresa

---
+++ database/factories/EmpleadoFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Empleado>
+ */
+class EmpleadoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+
+
+            "nombre" => $this->faker->name,
+            "email" => $this->faker->email,
+            "dni" => $this->faker->dni,
+            "telefono" => $this->faker->phoneNumber,
+            "salario" => $this->faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 1000000),
+        ];
+    }
+}
